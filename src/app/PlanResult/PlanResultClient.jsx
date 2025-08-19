99c1ed2d17498b0aaffc330a85ab66185b0a83f1
sethi12 <141761800+sethi12@users.noreply.github.com>
2025-08-19 18:41:51 +0530

    Create PlanResultClient.jsx

---
+++ src/app/PlanResult/PlanResultClient.jsx
@@ -0,0 +1,40 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+
+export default function PlanResultClient() {
+  const [message, setMessage] = useState("Checking payment...");
+  const searchParams = useSearchParams();
+  const order_id = searchParams.get("order_id");
+
+  useEffect(() => {
+    if (!order_id) return;
+
+    const verifyPayment = async () => {
+      try {
+        const res = await axios.get(
+          `https://inbredtechno-backend.onrender.com/api/payment/verify/${order_id}`
+        );
+
+        if (res.data.order_status === "PAID") {
+          setMessage("✅ Plan purchased successfully!");
+        } else {
+          setMessage("❌ No payment made or cancelled.");
+        }
+      } catch (err) {
+        console.error(err);
+        setMessage("⚠️ Could not verify payment.");
+      }
+    };
+
+    verifyPayment();
+  }, [order_id]);
+
+  return (
+    <div>
+      <h1>Payment Result</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
