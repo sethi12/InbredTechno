8a67b59b0719377f83c2f6f8bcd67de21a9a9fbd
sethi12 <141761800+sethi12@users.noreply.github.com>
2025-08-21 15:33:59 +0530

    Update page.jsx

--- src/app/Plan/page.jsx
+++ src/app/Plan/page.jsx
@@ -8,13 +8,13 @@ export default function Plan() {
   const initiatePayment = async () => {
     setLoading(true);
     try {
-      const order_id = `order_${Date.now()}`;
+      // const order_id = `order_${Date.now()}`;
 
       // Create order in backend
       const { payment_session_id } = (await axios.post(
         "https://inbredtechno-backend.onrender.com/api/payment/create-order",
         {
-          order_id,
+          // order_id,
           order_amount: 1,
           customer_id: "cust_001",
           customer_email: "test@example.com",
@@ -30,7 +30,7 @@ export default function Plan() {
       cf.checkout({
         paymentSessionId: payment_session_id,
         redirectTarget: "_self",
-        redirectUrl: `/plan-result?order_id=${order_id}`, // redirect to result page
+        redirectUrl: `/PlanResult?order_id=${order_id}`, // redirect to result page
       });
 
     } catch (err) {
